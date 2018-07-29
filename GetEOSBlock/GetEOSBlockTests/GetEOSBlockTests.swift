dc2d49129ba930fa3f2366608ea7ae15d5e7d0e8
Serguei Vinnitskii <svinnitskii@menupad.com>
2018-07-29 11:00:22 -0400

    added tests for get block and getTransactionActions

--- GetEOSBlock/GetEOSBlockTests/GetEOSBlockTests.swift
+++ GetEOSBlock/GetEOSBlockTests/GetEOSBlockTests.swift
@@ -26,7 +26,7 @@ class GetEOSBlockTests: XCTestCase {
         return URL(string: "\(self.basePath)/v1/chain/get_abi")!
     }
     
-    let trxId = "4d0c96e4dbf6691df87c966cf46fd5f5faa4a29ae12693179317d1c54880a43c" // voter, 1 action
+    let trxId1 = "4d0c96e4dbf6691df87c966cf46fd5f5faa4a29ae12693179317d1c54880a43c" // voter, 1 action
     let trxId2 = "4adb329489fdfdb43213a630e93dd13bf1f21b332efa67b9bd65bc0ca976c1cc" // ram, 3 actions
     let blockNum = "8157990"
 
@@ -39,11 +39,8 @@ class GetEOSBlockTests: XCTestCase {
             XCTAssertNil(error)
             exp.fulfill()
         }
-
         waitForExpectations(timeout: 5) { error in
-            if error != nil {
-                XCTFail("waitForExpectation timed out with error: \(error!)")
-            }
+            if error != nil { XCTFail("waitForExpectation timed out with error: \(error!)")}
         }
     }
     
@@ -57,11 +54,8 @@ class GetEOSBlockTests: XCTestCase {
             XCTAssertNil(error)
             exp.fulfill()
         }
-        
         waitForExpectations(timeout: 5) { error in
-            if error != nil {
-                XCTFail("waitForExpectation timed out with error: \(error!)")
-            }
+            if error != nil { XCTFail("waitForExpectation timed out with error: \(error!)") }
         }
     }
     
@@ -73,22 +67,44 @@ class GetEOSBlockTests: XCTestCase {
             XCTAssertNil(error)
             exp.fulfill()
         }
-        
         waitForExpectations(timeout: 5) { error in
-            if error != nil {
-                XCTFail("waitForExpectation timed out with error: \(error!)")
-            }
+            if error != nil { XCTFail("waitForExpectation timed out with error: \(error!)")}
         }
     }
     
+    func testGetBlock() {
+        
+        let exp = expectation(description: "Got block")
+        EOSAPI.current.getBlock(numberOrId: self.blockNum) { block, error in
+            XCTAssertNotNil(block)
+            XCTAssertNil(error)
+            exp.fulfill()
+        }
+        waitForExpectations(timeout: 5) { error in
+            if error != nil { XCTFail("waitForExpectation timed out with error: \(error!)")}
+        }
+    }
+    
+    func testGetTransactionActions() {
+        
+        let exp = expectation(description: "Got block")
+        EOSAPI.current.getTransactionActions(byId: self.trxId1) { actions, error in
+            XCTAssertNotNil(actions)
+            XCTAssertNil(error)
+            exp.fulfill()
+        }
+        waitForExpectations(timeout: 5) { error in
+            if error != nil { XCTFail("waitForExpectation timed out with error: \(error!)") }
+        }
+    }
+    
+    
+    
+    
+    
     func allMethods() {
         
-//        EOSAPI.current.fetchDataFrom(url: <#T##URL#>, completion: <#T##(Data?, APIError?) -> Void#>)
-//        EOSAPI.current.fetchData(fromURL: <#T##URL#>, withParameters: <#T##[String : String]#>, completion: <#T##(Data?, APIError?) -> Void#>)
-//
-//        EOSAPI.current.getChain(completion: <#T##(Chain?, APIError?) -> Void#>)
-//        EOSAPI.current.getBlock(numberOrId: <#T##String#>, completion: <#T##(Block?, APIError?) -> Void#>)
-//        EOSAPI.current.getTransactionActions(byId: <#T##String#>, completion: <#T##([Action]?, APIError?) -> Void#>)
+
 //        EOSAPI.current.getContract(forAction: <#T##Action#>, completion: <#T##(Contract?, APIError?) -> Void#>)
 //        EOSAPI.current.getContratsFor(actions: <#T##[Action]#>, completion: <#T##([Contract]?) -> Void#>)
 //        EOSAPI.current.renderContractsForActions(contracts: <#T##[Contract]#>, actions: <#T##[Action]#>)
